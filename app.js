a9ed8f5ae631d87d406f3b70b5ea2a29510a702e
db <diogo.bernardo.dev@gmail.com>
2023-04-16 14:44:21 +0100

    remove howler, trying web audio

--- app.js
+++ app.js
@@ -1,45 +1,69 @@
-const notes = {
-    C0: new Howl({ src: ["notes/1.wav"] }),
-    "C0#": new Howl({ src: ["notes/2.wav"] }),
-    D0: new Howl({ src: ["notes/3.wav"] }),
-    "D0#": new Howl({ src: ["notes/4.wav"] }),
-    E0: new Howl({ src: ["notes/5.wav"] }),
-    F0: new Howl({ src: ["notes/6.wav"] }),
-    "F0#": new Howl({ src: ["notes/7.wav"] }),
-    G0: new Howl({ src: ["notes/8.wav"] }),
-    "G0#": new Howl({ src: ["notes/9.wav"] }),
-    A0: new Howl({ src: ["notes/10.wav"] }),
-    "A0#": new Howl({ src: ["notes/11.wav"] }),
-    B0: new Howl({ src: ["notes/12.wav"] }),
-    C1: new Howl({ src: ["notes/13.wav"] }),
-    "C1#": new Howl({ src: ["notes/14.wav"] }),
-    D1: new Howl({ src: ["notes/15.wav"] }),
-    "D1#": new Howl({ src: ["notes/16.wav"] }),
-    E1: new Howl({ src: ["notes/17.wav"] }),
-    F1: new Howl({ src: ["notes/18.wav"] }),
-    "F1#": new Howl({ src: ["notes/19.wav"] }),
-    G1: new Howl({ src: ["notes/20.wav"] }),
-    "G1#": new Howl({ src: ["notes/21.wav"] }),
-    A1: new Howl({ src: ["notes/22.wav"] }),
-    "A1#": new Howl({ src: ["notes/23.wav"] }),
-    B1: new Howl({ src: ["notes/24.wav"] }),
-    C2: new Howl({ src: ["notes/25.wav"] }),
-    "C2#": new Howl({ src: ["notes/26.wav"] }),
-    D2: new Howl({ src: ["notes/27.wav"] }),
-    "D2#": new Howl({ src: ["notes/28.wav"] }),
-    E2: new Howl({ src: ["notes/29.wav"] }),
-    F2: new Howl({ src: ["notes/30.wav"] }),
-    "F2#": new Howl({ src: ["notes/31.wav"] }),
-    G2: new Howl({ src: ["notes/32.wav"] }),
-    "G2#": new Howl({ src: ["notes/33.wav"] }),
-    A2: new Howl({ src: ["notes/34.wav"] }),
-    "A2#": new Howl({ src: ["notes/35.wav"] }),
-    B2: new Howl({ src: ["notes/36.wav"] }),
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const gainNode = audioContext.createGain();
+gainNode.connect(audioContext.destination);
+
+const loadBuffer = async (url) => {
+    const response = await fetch(url);
+    const audioData = await response.arrayBuffer();
+    const buffer = await audioContext.decodeAudioData(audioData);
+    return buffer;
+};
+
+const playNote = async (note) => {
+    const source = audioContext.createBufferSource();
+    source.buffer = await loadBuffer(`notes/${note}.wav`);
+    source.connect(gainNode);
+    source.start(0);
 };
 
 document.querySelectorAll(".pianoBlackKey, .pianoWhiteKey").forEach((key) => {
-    key.addEventListener("mousedown", (e) => {
+    key.addEventListener("mousedown", async (e) => {
         const note = e.target.dataset.note;
-        notes[note].play();
+        await playNote(note);
     });
 });
+
+const analyser = audioContext.createAnalyser();
+gainNode.connect(analyser);
+
+analyser.fftSize = 4096;
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+
+analyser.getByteTimeDomainData(dataArray);
+
+const canvas = document.getElementById("canvas");
+const canvasCtx = canvas.getContext("2d");
+const WIDTH = canvas.width;
+const HEIGHT = canvas.height;
+
+function draw() {
+    requestAnimationFrame(draw);
+    analyser.getByteTimeDomainData(dataArray);
+    canvasCtx.fillStyle = "rgb(200, 200, 200)";
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = "rgb(0, 0, 200)";
+    canvasCtx.beginPath();
+
+    const sliceWidth = canvas.width / bufferLength;
+
+    let x = 0;
+
+    for (let i = 0; i < bufferLength; i++) {
+        const v = dataArray[i] / 128.0;
+        const y = v * (HEIGHT / 2);
+
+        if (i === 0) {
+            canvasCtx.moveTo(x, y);
+        } else {
+            canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+    }
+    canvasCtx.lineTo(WIDTH, HEIGHT / 2);
+    canvasCtx.stroke();
+}
+
+draw();
